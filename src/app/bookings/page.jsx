be136736fef3e8c1a759172b6c2ebc8278413e39
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-17 09:47:27 +0600

    Bookings page design started

---
+++ src/app/bookings/page.jsx
@@ -0,0 +1,12 @@
+import MyContainer from "@/components/Common/MyContainer";
+import React from "react";
+
+const BookingsPage = () => {
+  return (
+    <MyContainer className="pt-35 pb-20">
+      <p>Bookings Page</p>
+    </MyContainer>
+  );
+};
+
+export default BookingsPage;
