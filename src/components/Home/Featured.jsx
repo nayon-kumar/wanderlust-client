662da0a3fc0bce0abd502beca0d62ba227ea2c1c
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-28 10:43:12 +0600

    Title and button designed

--- src/components/Home/Featured.jsx
+++ src/components/Home/Featured.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import { Button } from "@heroui/react";
+import { FaArrowRight } from "react-icons/fa";
+import MyContainer from "../Common/MyContainer";
 
 const Featured = () => {
   return (
-    <div>
-      <p>Featured Destinations</p>
-    </div>
+    <MyContainer className="py-20">
+      <div className="flex flex-wrap items-center justify-center sm:justify-between gap-4">
+        <div className="text-center sm:text-left">
+          <h3 className="text-[#0C0B0B] text-2xl font-semibold md:text-4xl">
+            Featured Destinations
+          </h3>
+          <p className="mt-1 text-[#6C696D] ">
+            Handpicked travel experiences for the adventure seekers
+          </p>
+        </div>
+        <div>
+          <Button>
+            All destinations <FaArrowRight />{" "}
+          </Button>
+        </div>
+      </div>
+      <div></div>
+    </MyContainer>
   );
 };
 
