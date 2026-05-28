b65c34ad954c960b22ca8f2b7050be02b518e0a4
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-28 10:30:40 +0600

    Featured Destinations section design started

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Banner from "@/components/Home/Banner";
+import Featured from "@/components/Home/Featured";
 
 export default function Home() {
   return (
     <div>
       <main>
         <Banner />
+        <Featured />
       </main>
     </div>
   );
--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -62,7 +62,7 @@ const Footer = () => {
             <h3 className="text-white mb-3 tracking-wide">CONTACT US</h3>
             <ul className="space-y-2">
               <li>786 901 1622</li>
-              <li>info@wandarland.com</li>
+              <li>info@wanderlust.com</li>
             </ul>
           </div>
         </div>
